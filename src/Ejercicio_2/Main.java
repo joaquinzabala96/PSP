33e12410952bc08b8e26a485a01b0bf9dfb2a833
mastermartin11 <43747611+mastermartin11@users.noreply.github.com>
2018-11-12 11:26:34 +0100

    introducido ejercicio 2

--- src/Ejercicio_2/Main.java
+++ src/Ejercicio_2/Main.java
@@ -1,10 +1,33 @@
 package Ejercicio_2;
+import java.util.*;
 
 public class Main {
 
+	
+	static Scanner cin= new Scanner(System.in);
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		String [] abc= {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","Ñ","O","P","Q","R","S","T","U","V","W",
+				"X","Y","Z"};
+		
+		
+		System.out.println("Num:");
+		int num=cin.nextInt();
+		
+		for(int x=0;x<num;x++) {
+			String result="";
+		int random1= (int) (Math.random()*21);
+		for(int i=0;i<random1;i++) {
+			int random2= (int) (Math.random()*21);
+			result+=abc[random2];
+		}System.out.println(result);}
+		
+		
+		
 
 	}
+
+}
 
 }
